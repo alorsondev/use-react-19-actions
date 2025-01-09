7cf9e06647b0ff34d3400a04f33b6ea8f1b7d2c7
Abeba NGWE <abeba.ngwe@adevinta.com>
2025-01-09 01:06:17 +0100

    refactor(action): use useActionState

--- src/app/EnterWord.tsx
+++ src/app/EnterWord.tsx
@@ -1,30 +1,23 @@
 'use client'
 
-import { useState } from 'react'
+import { useActionState } from 'react'
+
 import { fakeApiCall } from './api'
 
 function EnterWord() {
-  const [isPending, setIsPending] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('')
-  const [successMessage, setSuccessMessage] = useState('')
-
-  const submitAction = async (formData: FormData) => {
-    setIsPending(true)
-    setErrorMessage('')
-
+  const submitAction = async (prevState, formData: FormData) => {
     try {
       const response = await fakeApiCall(formData.get('wordInput') as string)
-      setSuccessMessage(response as string)
+      return { successMessage: response as string }
     } catch (err) {
-      setErrorMessage(err as string)
-      setSuccessMessage('')
-    } finally {
-      setIsPending(false)
+      return { errorMessage: err as string }
     }
   }
 
+  const [formState, formAction, isPending] = useActionState(submitAction, null)
+
   return (
-    <form action={submitAction} className="flex flex-col items-start">
+    <form action={formAction} className="flex flex-col items-start">
       <div className="flex flex-col items-start gap-2 mb-2">
         <label htmlFor="wordInput">Saisissez un mot:</label>
         <input
@@ -38,8 +31,8 @@ function EnterWord() {
       <button type="submit" disabled={isPending} className="rounded p-2 bg-cyan-600 text-white">
         {isPending ? 'Chargement...' : 'Envoyer'}
       </button>
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      {successMessage && <p>{successMessage}</p>}
+      {formState?.errorMessage && <p style={{ color: 'red' }}>{formState.errorMessage}</p>}
+      {formState?.successMessage && <p>{formState.successMessage}</p>}
     </form>
   )
 }
